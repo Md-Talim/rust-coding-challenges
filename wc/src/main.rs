30c7e9f1b6c20023a269fd7e55340fbba8b07e04
Md. Talim <talimmohammad116@gmail.com>
2025-09-19 23:23:08 +0530

    wc: implement step 2 (-l flag for no. of lines)

--- wc/src/main.rs
+++ wc/src/main.rs
@@ -1,15 +1,10 @@
-use std::{env, fs};
+use std::{
+    env,
+    fs::{self, File},
+    io::{BufRead, BufReader},
+};
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    if args.len() != 3 || args[1] != "-c" {
-        eprintln!("Usage: {} -c <filename>", args[0]);
-        std::process::exit(1);
-    }
-
-    let filename = &args[2];
-
+fn count_bytes(filename: &String) {
     match fs::read(filename) {
         Ok(data) => {
             println!("{:8} {}", data.len(), filename);
@@ -18,5 +13,46 @@ fn main() {
             eprintln!("Error reading file {}: {}", e, filename);
             std::process::exit(1);
         }
+    }
+}
+
+fn count_lines(filename: &String) {
+    let file = match File::open(filename) {
+        Ok(f) => f,
+        Err(e) => {
+            eprintln!("Error opening file {}: {}", filename, e);
+            std::process::exit(1);
+        }
+    };
+
+    let reader = BufReader::new(file);
+    let mut line_count = 0;
+    for line in reader.lines() {
+        match line {
+            Ok(_) => line_count += 1,
+            Err(e) => {
+                eprintln!("Error reading line: {}", e);
+                std::process::exit(1);
+            }
+        }
+    }
+    println!("{:8} {}", line_count, filename);
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 3 {
+        eprintln!("Usage: {} -c/-l <filename>", args[0]);
+        std::process::exit(1);
+    }
+
+    let flag = &args[1];
+    let filename = &args[2];
+
+    match flag.as_str() {
+        "-c" => count_bytes(filename),
+        "-l" => count_lines(filename),
+        _ => println!("Unknown option {}", flag),
     }
 }
