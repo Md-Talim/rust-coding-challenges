7c4fc3086cc6bc5f68e1744b9ec6688070f2920e
Md. Talim <talimmohammad116@gmail.com>
2025-09-20 21:20:17 +0530

    wc: add support for -m flag

--- wc/src/main.rs
+++ wc/src/main.rs
@@ -1,33 +1,71 @@
 use std::{
     env,
-    fs::{self, File},
-    io::{BufRead, BufReader},
+    fs::File,
+    io::{BufReader, Read},
 };
 
 struct FileStats {
     bytes: u64,
     lines: usize,
     words: usize,
+    chars: usize,
 }
 
 fn compute_stats(filename: &String) -> Result<FileStats, std::io::Error> {
     let file = File::open(filename)?;
-    let reader = BufReader::new(file);
+    let bytes = file.metadata()?.len();
 
-    let bytes = fs::metadata(filename)?.len();
+    let mut reader = BufReader::new(file);
+    let mut buffer = [0; 4096]; // read in 4KB chunks
+
     let mut lines = 0;
     let mut words = 0;
+    let mut chars = 0;
 
-    for line_result in reader.lines() {
-        let line = line_result?;
-        lines += 1;
-        words += line.split_whitespace().count();
+    let mut in_word = false;
+    let mut leftover = Vec::new();
+
+    loop {
+        let n = reader.read(&mut buffer)?;
+        if n == 0 {
+            break;
+        }
+
+        let mut chunk = leftover.clone();
+        chunk.extend_from_slice(&buffer[..n]);
+
+        let (valid_str, valid_up_to) = match std::str::from_utf8(&chunk) {
+            Ok(s) => (s, chunk.len()),
+            Err(e) => (
+                std::str::from_utf8(&chunk[..e.valid_up_to()]).unwrap(),
+                e.valid_up_to(),
+            ),
+        };
+
+        for c in valid_str.chars() {
+            chars += 1;
+
+            if c == '\n' {
+                lines += 1;
+            }
+
+            if c.is_whitespace() {
+                in_word = false;
+            } else if !in_word {
+                words += 1;
+                in_word = true;
+            }
+        }
+
+        leftover.clear();
+        leftover.extend_from_slice(&chunk[valid_up_to..]);
     }
 
     return Ok(FileStats {
         bytes,
         lines,
         words,
+        chars,
     });
 }
 
@@ -35,7 +73,7 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     if args.len() != 3 {
-        eprintln!("Usage: {} -c/-l <filename>", args[0]);
+        eprintln!("Usage: {} -c/-l/-w/-m <filename>", args[0]);
         std::process::exit(1);
     }
 
@@ -47,6 +85,7 @@ fn main() {
             "-c" => println!("{:8} {}", stats.bytes, filename),
             "-l" => println!("{:8} {}", stats.lines, filename),
             "-w" => println!("{:8} {}", stats.words, filename),
+            "-m" => println!("{:8} {}", stats.chars, filename),
             _ => println!("Unknown option {}", flag),
         },
         Err(e) => eprintln!("Error: {}", e),
@@ -65,5 +104,6 @@ mod tests {
         assert_eq!(stats.bytes, 342190);
         assert_eq!(stats.lines, 7145);
         assert_eq!(stats.words, 58164);
+        assert_eq!(stats.chars, 339292);
     }
 }
